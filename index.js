739248dcb84dcc4522c3626150b2ef5f88ba8bd3
Ivan Nikolić <niksy5@gmail.com>
2014-11-22 16:54:02 +0100

    Move main file to root and rename to index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports.constructClasses = require('./src/constructClasses');
+module.exports.eventEmitter = require('./src/eventEmitter');
--- src/kist-pluginator.js
+++
@@ -1,2 +0,0 @@
-module.exports.constructClasses = require('./constructClasses');
-module.exports.eventEmitter = require('./eventEmitter');
