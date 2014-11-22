76aa49b167888e55f6e82a1b2f9ffa7e4d92f66e
Ivan Nikolić <niksy5@gmail.com>
2014-11-22 12:00:11 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function ( grunt ) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: ['pkg'],
+				commit: true,
+				commitMessage: 'Release %VERSION%',
+				commitFiles: ['-a'],
+				createTag: true,
+				tagName: '%VERSION%',
+				tagMessage: '',
+				push: false
+			}
+		},
+
+		jscs: {
+			main: {
+				options: {
+					config: '.jscsrc'
+				},
+				files: {
+					src: [
+						'src/**/*.js'
+					]
+				}
+			}
+		},
+
+		jshint: {
+			main: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: [
+					'src/**/*.js'
+				]
+			}
+		},
+
+		karma: {
+			main: {
+				configFile: 'karma.conf.js'
+			}
+		}
+
+	});
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('test', ['karma:main']);
+	grunt.registerTask('stylecheck', ['jshint:main', 'jscs:main']);
+	grunt.registerTask('default', ['stylecheck']);
+	grunt.registerTask('releasePatch', ['bump-only:patch', 'default', 'bump-commit']);
+	grunt.registerTask('releaseMinor', ['bump-only:minor', 'default', 'bump-commit']);
+	grunt.registerTask('releaseMajor', ['bump-only:major', 'default', 'bump-commit']);
+
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+# kist-pluginator
+
+Helpers for my jQuery plugins.
+
+## Installation
+
+```sh
+bower install niksy/kist-pluginator
+```
+
+<!-- Here goes your documentation -->
+
+## License
+
+MIT © [Ivan Nikolić](http://ivannikolic.com)
