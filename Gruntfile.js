5187967d2aa2b2bd51ecf506fc52f929974ca6c5
Ivan Nikolić <niksy5@gmail.com>
2014-11-22 17:01:54 +0100

    Update package.json and Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function ( grunt ) {
 
 		bump: {
 			options: {
-				files: ['package.json', 'bower.json'],
+				files: ['package.json'],
 				updateConfigs: ['pkg'],
 				commit: true,
 				commitMessage: 'Release %VERSION%',
