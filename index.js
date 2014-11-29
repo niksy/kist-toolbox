268d9e8fcaceb3cf26d18ebbb91b8bfd963d1ae0
Ivan Nikolić <niksy5@gmail.com>
2014-11-29 12:40:03 +0100

    Refactor main file

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-module.exports.constructClasses = require('./src/constructClasses');
-module.exports.eventEmitter = require('./src/eventEmitter');
+module.exports = {
+	constructClasses: require('./src/constructClasses'),
+	constructOptions: require('./src/constructOptions'),
+	eventEmitter: require('./src/eventEmitter'),
+	isPublicMethod: require('./src/isPublicMethod'),
+	runPlugin: require('./src/runPlugin')
+};
