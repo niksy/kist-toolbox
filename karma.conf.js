96ee898c34d8108e37555af5eff07ba9e8228749
Ivan Nikolić <niksy5@gmail.com>
2014-11-29 12:38:21 +0100

    Add support for IE8

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'IE8 - WinXP'],
 
 
     // Continuous Integration mode
